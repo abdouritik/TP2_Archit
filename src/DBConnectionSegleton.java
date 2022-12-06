8c9e145cb9eae92f6d693b13d78afd07bb526c98
unknown <boulberhane.abderrahmen@gmail.com>
2022-12-06 20:18:17 +0000

    commentaire1

---
+++ src/DBConnectionSegleton.java
@@ -0,0 +1,15 @@
+
+public class DBConnectionSegleton {
+	private static DBConnectionSegleton instance = new DBConnectionSegleton();
+	
+	private DBConnectionSegleton() {
+		
+	}
+	
+	public static DBConnectionSegleton getInstance() {
+		return instance;
+	}
+	
+	
+
+}
